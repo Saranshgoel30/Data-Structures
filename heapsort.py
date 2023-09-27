6c70a418faeb3fe5a8f086e05e2ecb9e74a9287a
Saransh Goel <saransh.goel_ug25@ashoka.edu.in>
2023-09-27 20:09:10 +0530

    saving changed code

---
+++ heapsort.py
@@ -0,0 +1,10 @@
+# heap sort algorithm
+# Time Complexity: O(nlogn)
+
+def sort():
+    for i in range(6, -1, -1):
+        max_heapify(i)
+
+# Assuming you have a max_heapify function defined elsewhere.
+
+
